1be413351c9b5e9d0d0f769927038dc5acd9f554
KyRillos BoshRa <kyrilosboshra@gmail.com>
2022-10-20 23:24:09 +0200

    adding cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from . import models, database
 from .routers import user, post, auth, like
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.get('/')
 async def root():
