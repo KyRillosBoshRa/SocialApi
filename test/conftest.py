6253c7e2bad08c50edc48afd64a9081f54968160
KyRillos BoshRa <kyrilosboshra@gmail.com>
2022-11-23 13:40:36 +0200

    testing user

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,7 @@ from app.main import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.database import get_db, Base, SQLALCHEMY_DATABASE_URL
+from app import schemas
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL+'_test')
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -27,3 +28,12 @@ def client(session):
       session.close()
   app.dependency_overrides[get_db] = override_get_db
   yield TestClient(app)
+
+@pytest.fixture
+def test_user(client):
+  user_data = schemas.UserCreate(email='x@y.z', password='xyz')
+  res = client.post('/users/', json=user_data.dict())
+  assert res.status_code == 201
+  res = res.json()
+  res['password'] = 'xyz'
+  return res
--- test/test_users.py
+++ test/test_users.py
@@ -1,8 +1,29 @@
-from app.schemas import UserResponse
-from .database import client, session
+import pytest
+from app.config import settings
+from app import schemas
+from jose import jwt
+
 
 async def test_create_user(client):
   res = client.post('/users/', json={'email': 'z@gmail.com', 'password': '1234'})
-  new_user = UserResponse(**res.json())
+  new_user = schemas.UserResponse(**res.json())
   assert new_user.email == 'z@gmail.com'
   assert res.status_code == 201
+
+async def test_login_user(test_user, client):
+  res = client.post('/login', data={'username': test_user['email'], 'password': test_user['password']})
+  token = schemas.Token(**res.json())
+  payload = jwt.decode(token.access_token, settings.secret_key, algorithms=[settings.algorithm])
+  assert payload.get('id') == test_user['id']
+  assert res.status_code == 200
+
+@pytest.mark.parametrize('email, password, status_code', [
+  ('wmail', '1234', 403),
+  ('z@gmail.com', 'wp', 403),
+  ('wmail', 'wp', 403),
+  (None, '1234', 422),
+  ('z@gmail.com', None, 422)
+])
+async def test_invalid_login(test_user, client, email, password, status_code):
+  res = client.post('/login', data={'username': email, 'password': password})
+  assert res.status_code == status_code
